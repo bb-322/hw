2e9cd236c7f29995b455ad0cb599ee0d7ef8a6e5
ThinBug <mick.borysenko@gmail.com>
2025-05-15 12:40:36 +0300

    task 1 fixed

--- hw2/hw2_1.py
+++ hw2/hw2_1.py
@@ -1,29 +1,22 @@
 class Editor:
-    def __init__(self, key:int):
-        self.key = key
-        self.key_pro = 982534892
-    
     def view_document(self):
         return 'document' 
 
     def edit_document(self):
-        if self.key != self.key_pro:
-            return 'Document editing is only available with Pro version'
-        else:
-            return ProEditor(Editor).edit_document()
+        return 'Document editing is only available with Pro version'
     
 
 class ProEditor(Editor):
     def edit_document(self):
-        super().edit_document()
         return 'Editing document'
 
+key_input = int(input("Enter key: "))
+key = 982534892
 
-user = Editor(1)
-admin = Editor(982534892)
-
-print(user.edit_document())
-print(admin.edit_document())
+if key_input == key:
+    user = ProEditor()
+else:
+    user = Editor()
 
 print(user.view_document())
-print(admin.view_document())
+print(user.edit_document())
